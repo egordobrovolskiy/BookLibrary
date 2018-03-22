81c2fc9fd8de456ac6b77e2d3a770d4476604288
Egor Dobrovolskiy <egordobrovolskiy@gmail.com>
2018-03-22 22:22:49 +0200

    Search by letter

--- src/main/java/com/dobro/spring/library/objects/LibraryFacade.java
+++ src/main/java/com/dobro/spring/library/objects/LibraryFacade.java
@@ -3,26 +3,35 @@ package com.dobro.spring.library.objects;
 import com.dobro.spring.library.dao.interfaces.BookDAO;
 import com.dobro.spring.library.entities.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@Scope("singleton")
 public class LibraryFacade {
 
 
+    @Autowired
     private BookDAO bookDAO;
 
     @Autowired
-    public void setBookDAO(BookDAO bookDAO) {
-        this.bookDAO = bookDAO;
-        books = bookDAO.getBooks();
-    }
+    private SearchCriteria searchCriteria;
 
     private List<Book> books;
 
 
     public List<Book> getBooks() {
+        if (books == null){
+            books = bookDAO.getBooks();
+        }
         return books;
     }
+
+    public void searchBooksByLetter() {
+        books = bookDAO.getBooks(searchCriteria.getLetter());
+    }
+
+
 }
